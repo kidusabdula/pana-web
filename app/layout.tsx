c20d4102d54297f8c336952f6875a9eff0781544
cAlEb <101443174+caleb-cactus@users.noreply.github.com>
2025-11-15 22:58:56 +0300

    theme: change default theme from light to dark

--- app/layout.tsx
+++ app/layout.tsx
@@ -24,7 +24,7 @@ export default function RootLayout({
   return (
     <html lang="en" suppressHydrationWarning>
       <body className={inter.className}>
-        <ThemeProvider attribute="class" defaultTheme="light" enableSystem>
+        <ThemeProvider attribute="class" defaultTheme="dark" enableSystem>
           <div className="min-h-screen flex flex-col">
             <Header />
             <main className="flex-1">{children}</main>
